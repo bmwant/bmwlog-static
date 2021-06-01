3cadc9ed078d1910992b9ca3e461762248d6a544
Misha Behersky <bmwant@gmail.com>
2021-06-01 20:28:42 +0300

    Add lowercase script :dvd:

---
+++ lowercase_posts.py
@@ -0,0 +1,15 @@
+import os
+import shutil
+
+
+def lowercase_filenames():
+    dir_path = 'source/_drafts'
+    for filename in os.listdir(dir_path):
+        old_filepath = os.path.join(dir_path, filename)
+        new_filepath = os.path.join(dir_path, filename.lower())
+        print(new_filepath)
+        shutil.move(old_filepath, new_filepath)
+
+
+if __name__ == '__main__':
+    lowercase_filenames()
