c039aedbaaab630530bfb5d14f4d2a80df88af20
Misha Behersky <bmwant@gmail.com>
2021-06-23 18:45:38 +0300

    Try shuffling with names :pizza:

--- lowercase_posts.py
+++ lowercase_posts.py
@@ -3,10 +3,10 @@ import shutil
 
 
 def lowercase_filenames():
-    dir_path = 'source/_posts'
+    dir_path = 'source/_drafts'
     for filename in os.listdir(dir_path):
         old_filepath = os.path.join(dir_path, filename)
-        new_filepath = os.path.join(dir_path, filename.lower())
+        new_filepath = f'{os.path.join(dir_path, filename.lower())}-1'
         print(new_filepath)
         shutil.move(old_filepath, new_filepath)
 
