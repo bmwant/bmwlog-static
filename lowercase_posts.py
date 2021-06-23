4cffe4541918733905ab5322d438f9f29020af82
Misha Behersky <bmwant@gmail.com>
2021-06-23 18:47:30 +0300

    Finally proper naming for posts :jeans:

--- lowercase_posts.py
+++ lowercase_posts.py
@@ -6,7 +6,7 @@ def lowercase_filenames():
     dir_path = 'source/_posts'
     for filename in os.listdir(dir_path):
         old_filepath = os.path.join(dir_path, filename)
-        new_filepath = f'{os.path.join(dir_path, filename.lower())}-1'
+        new_filepath = os.path.join(dir_path, filename.lower())[:-2]
         print(new_filepath)
         shutil.move(old_filepath, new_filepath)
 
